5f6eef0e26031bc1c5baf800f457995de7c80db0
hdsuperman <zhd.superman@gmail.com>
2024-08-11 22:19:18 +0800

    feat: add store

--- app/stores/use-post-store.ts
+++ app/stores/use-post-store.ts
@@ -1,3 +1,28 @@
-export function usePostStore() {
+import { create } from 'zustand';
+import { immer } from 'zustand/middleware/immer';
+import { Post } from '~/type';
 
+interface Store {
+  ids: string[];
+  entities: Record<string, Post | undefined>;
+  setPosts: (posts: Post[]) => void;
+}
+
+export const usePostStore = create(
+  immer<Store>(set => ({
+    ids: [],
+    entities: {},
+    setPosts: posts =>
+      set(state => {
+        state.ids = posts.map(p => p.id);
+        state.entities = posts.reduce((acc, post) => {
+          acc[post.id] = post;
+          return acc;
+        }, state.entities);
+      })
+  }))
+);
+
+export function usePostById(postId: string): Post | undefined {
+  return usePostStore(state => state.entities[postId]);
 }

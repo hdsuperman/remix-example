cb18b64edfa47a55be6dac7cb756ae60b47b8258
hdsuperman <zhd.superman@gmail.com>
2024-08-11 22:10:19 +0800

    feat: add react-query

--- app/root.tsx
+++ app/root.tsx
@@ -4,10 +4,25 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
-import "./tailwind.css";
+} from '@remix-run/react';
+import { ReactNode, useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import './tailwind.css';
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export function Layout({ children }: { children: ReactNode }) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // With SSR, we usually want to set some default staleTime
+            // above 0 to avoid refetching immediately on the client
+            staleTime: 60 * 1000,
+          },
+        },
+      }),
+  )
+
   return (
     <html lang="en">
       <head>
@@ -17,7 +32,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
         <ScrollRestoration />
         <Scripts />
       </body>
--- app/routes/posts.$postId.tsx
+++ app/routes/posts.$postId.tsx
@@ -1,10 +1,12 @@
-import { ClientLoaderFunctionArgs, Link, useLoaderData } from '@remix-run/react';
+import { ClientLoaderFunctionArgs, Link, useLoaderData, useParams } from '@remix-run/react';
 import { LoaderFunctionArgs } from '@remix-run/node';
 import invariant from "tiny-invariant";
 import type { Post } from '~/type';
+import { useQuery } from '@tanstack/react-query';
 
 export const loader = async ({ params }: LoaderFunctionArgs): Promise<Post> => {
   invariant(params.postId, 'postId is required');
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   return { id: params.postId, title: `Post ${params.postId} from server loader` };
 }
 
@@ -14,14 +16,26 @@ export const clientLoader = async ({ params }: ClientLoaderFunctionArgs): Promis
 }
 
 export default function Post() {
+  const params = useParams<{ postId: string; }>();
   const post = useLoaderData<typeof loader>();
+
+  const { data } = useQuery({
+    initialData: post,
+    staleTime: 0,
+    queryKey: ['posts', params.postId],
+    queryFn: async () => {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      return { id: params.postId, title: `Post ${params.postId} from client query`};
+    }
+  });
 
   return (
     <div className="p-24">
       <div className="text-2xl mb-4">
         <Link to="/">Go to home</Link>
       </div>
-      <div>{post.title}</div>
+      <div>Loader title: {post.title}</div>
+      <div>Query title: {data.title}</div>
     </div>
   )
 }

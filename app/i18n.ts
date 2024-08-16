52e62bf71b6c9445dbdb218fee26ad71173e20d0
hdsuperman <zhd.superman@gmail.com>
2024-08-16 23:14:15 +0800

    feat: theme

--- app/i18n.ts
+++ app/i18n.ts
@@ -7,6 +7,7 @@ import zh from '@/locales/zh';
 export const resources = { en, zh } as const;
 export const defaultNS = 'common';
 
+// eslint-disable-next-line import/no-named-as-default-member
 i18next
   .use(initReactI18next)
   .use(LanguageDetector)
@@ -21,6 +22,7 @@ i18next
   .then(() => console.log('i18next initialized'));
 
 export async function changeLanguage(lng: string) {
+  // eslint-disable-next-line import/no-named-as-default-member
   await i18next.changeLanguage(lng);
 }
 
